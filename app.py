77640d41e7660fd2727cd15811bd5004fb49b21d
MoneraCRVG <m3ktrefe0122@gmail.com>
2024-11-14 21:33:02 -0400

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("print")
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Calculadora IMC"
+
+    altura_field = ft.TextField(label="Informe a altura")
+    peso_field = ft.TextField(label="Informe o peso")
+    result_text = ft.Text(
+        value="Insira as informações para começar!",
+        size=22,
+        width=150
+    )
+
+    result_img = ft.Image(src="src/img.png", width=150, height=150)
+    page.add(result_text, result_img, altura_field, peso_field)
+
+
+ft.app(main)
